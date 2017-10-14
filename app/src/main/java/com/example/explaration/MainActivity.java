67aedeb4c97c5b3682d123a6878e999df43537c3
H4rryA <harryarakkal@gmail.com>
2017-10-14 18:20:26 -0400

    Downgraded Gradle Version Integrated initial version of unity module.

--- app/src/main/java/com/example/explaration/MainActivity.java
+++ app/src/main/java/com/example/explaration/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.explaration;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 
+import com.explARation.UnityPlayerActivity;
 import com.google.android.gms.location.places.GeoDataClient;
 import com.google.android.gms.location.places.PlaceDetectionClient;
 import com.google.android.gms.location.places.Places;
@@ -14,6 +16,7 @@ import com.google.android.gms.location.places.Places;
 public class MainActivity extends AppCompatActivity {
     static int REQUEST_ACCESS_LOCATION = 1001;
     static int REQUEST_INTERNET = 1002;
+    static int REQUEST_CAMERA = 1003;
 
     protected GeoDataClient mGeoDataClient;
     protected PlaceDetectionClient mPlaceDetectionClient;
@@ -40,6 +43,12 @@ public class MainActivity extends AppCompatActivity {
                     new String[]{Manifest.permission.INTERNET},
                     REQUEST_INTERNET);
         }
+        if(ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA},
+                    REQUEST_CAMERA);
+        }
 
         mGeoDataClient = Places.getGeoDataClient(this, null);
         mPlaceDetectionClient = Places.getPlaceDetectionClient(this, null);
@@ -47,8 +56,9 @@ public class MainActivity extends AppCompatActivity {
         com.example.explaration.Places p = new com.example.explaration.Places(getBaseContext());
         p.findNearby("food");
 
-        //place1, place2, place3
-        //angle1, angle1, angle3
+        Intent intent = new Intent(getBaseContext(), UnityPlayerActivity.class);
+        intent.putExtra("Places", p);
+        startActivity(intent);
 
     }
 
